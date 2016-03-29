8ee4a83d2e389e4c7184fcd7839ffdf4a92e94ca
liuguangjie <1316421727@qq.com>
2016-03-29 18:23:58 +0800

    第一次提交

---
+++ src/main/java/test/ReadExcel2.java
@@ -0,0 +1,190 @@
+package test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel2 {
+
+	/**
+	 * 对外提供读取excel 的方法
+	 */
+	public static List<List<Object>> readExcel(File file) throws IOException {
+		String fileName = file.getName();
+		String extension = fileName.lastIndexOf(".") == -1 ? "" : fileName.substring(fileName.lastIndexOf(".") + 1);
+		if ("xls".equals(extension)) {
+			return read2003Excel(file);
+		} else if ("xlsx".equals(extension)) {
+			return read2007Excel(file);
+		} else {
+			throw new IOException("不支持的文件类型");
+		}
+	}
+
+	/**
+	 * 读取 office 2003 excel
+	 * 
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	private static List<List<Object>> read2003Excel(File file) throws IOException {
+		List<List<Object>> list = new LinkedList<List<Object>>();
+		HSSFWorkbook hwb = new HSSFWorkbook(new FileInputStream(file));
+		HSSFSheet sheet = hwb.getSheetAt(0);
+		Object value = null;
+		HSSFRow row = null;
+		HSSFCell cell = null;
+		int counter = 0;
+		for (int i = sheet.getFirstRowNum(); counter < sheet.getPhysicalNumberOfRows(); i++) {
+			row = sheet.getRow(i);
+			if (row == null) {
+				continue;
+			} else {
+				counter++;
+			}
+			List<Object> linked = new LinkedList<Object>();
+			for (int j = row.getFirstCellNum(); j <= row.getLastCellNum(); j++) {
+				cell = row.getCell(j);
+				if (cell == null) {
+					continue;
+				}
+				DecimalFormat df = new DecimalFormat("0");// 格式化 number String
+															// 字符
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 格式化日期字符串
+				DecimalFormat nf = new DecimalFormat("0.00");// 格式化数字
+				switch (cell.getCellType()) {
+				case XSSFCell.CELL_TYPE_STRING:
+					System.out.println(i + "行" + j + " 列 is String type");
+					value = cell.getStringCellValue();
+					break;
+				case XSSFCell.CELL_TYPE_NUMERIC:
+					System.out.println(
+							i + "行" + j + " 列 is Number type ; DateFormt:" + cell.getCellStyle().getDataFormatString());
+					if ("@".equals(cell.getCellStyle().getDataFormatString())) {
+						value = df.format(cell.getNumericCellValue());
+					} else if ("General".equals(cell.getCellStyle().getDataFormatString())) {
+						value = nf.format(cell.getNumericCellValue());
+					} else {
+						value = sdf.format(HSSFDateUtil.getJavaDate(cell.getNumericCellValue()));
+					}
+					break;
+				case XSSFCell.CELL_TYPE_BOOLEAN:
+					System.out.println(i + "行" + j + " 列 is Boolean type");
+					value = cell.getBooleanCellValue();
+					break;
+				case XSSFCell.CELL_TYPE_BLANK:
+					System.out.println(i + "行" + j + " 列 is Blank type");
+					value = "";
+					break;
+				default:
+					System.out.println(i + "行" + j + " 列 is default type");
+					value = cell.toString();
+				}
+				if (value == null || "".equals(value)) {
+					continue;
+				}
+				linked.add(value);
+			}
+			list.add(linked);
+		}
+		return list;
+	}
+
+	/**
+	 * 读取Office 2007 excel
+	 */
+	private static List<List<Object>> read2007Excel(File file) throws IOException {
+		List<List<Object>> list = new LinkedList<List<Object>>();
+		// 构造 XSSFWorkbook 对象，strPath 传入文件路径
+		XSSFWorkbook xwb = new XSSFWorkbook(new FileInputStream(file));
+		// 读取第一章表格内容
+		XSSFSheet sheet = xwb.getSheetAt(0);
+		// 用来装一行的内容
+		Object value = null;
+		XSSFRow row = null;
+		XSSFCell cell = null;
+		int counter = 0;
+		// 获取 一共有多少行
+		int rows = sheet.getPhysicalNumberOfRows();
+		// 开始行 角标从0开始
+		int startRow = sheet.getFirstRowNum();
+		for (int i = startRow; counter < rows; i++) {
+			row = sheet.getRow(i);
+			if (row == null) {
+				continue;
+			} else {
+				counter++;
+			}
+			// 一行的第0列是开始角标
+			int firstCell = row.getFirstCellNum();
+			// 一行的最后一列是结束
+			int laseCell = row.getLastCellNum();
+			List<Object> linked = new LinkedList<Object>();
+			for (int j = firstCell; j <= laseCell; j++) {
+				cell = row.getCell(j);
+				if (cell == null) {
+					continue;
+				}
+//				DecimalFormat df = new DecimalFormat("0");// 格式化 number String
+//				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 格式化日期字符串
+//				DecimalFormat nf = new DecimalFormat("0.00");// 格式化数字
+				// 判断列的内容类型
+				switch (cell.getCellType()) {
+				case XSSFCell.CELL_TYPE_STRING:
+					value = cell.getStringCellValue();
+					break;
+				case XSSFCell.CELL_TYPE_NUMERIC:
+					/*String dateString = cell.getCellStyle().getDataFormatString();
+					if ("@".equals(dateString)) {
+						value = df.format(cell.getNumericCellValue());
+					} else if ("GENERAL".equals(dateString)) {
+						value = nf.format(cell.getNumericCellValue());
+					} else {
+						value = sdf.format(HSSFDateUtil.getJavaDate(cell.getNumericCellValue()));
+					}*/
+					value=cell.getRawValue();
+					break;
+				case XSSFCell.CELL_TYPE_BOOLEAN:
+					value = cell.getBooleanCellValue();
+					break;
+				// 这一列为空
+				case XSSFCell.CELL_TYPE_BLANK:
+					value = "";
+					break;
+				default:
+					value = cell.toString();
+				}
+				if (value == null || "".equals(value)) {
+					continue;
+				}
+				linked.add(value);
+			}
+			list.add(linked);
+		}
+		return list;
+	}
+
+	public static void main(String[] args) {
+		try {
+			List<List<Object>> lists = readExcel(new File("/home/reet/Desktop/library.xlsx"));
+			System.out.println(lists);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
